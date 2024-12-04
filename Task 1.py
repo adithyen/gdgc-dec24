c3cac333e85cf2905a8580293cf08c51873e09cf
adithyen <adityenh@gmail.com>
2024-12-04 19:50:18 +0530

    Updated Task 1.py

--- Task 1.py
+++ Task 1.py
@@ -1,3 +1,10 @@
+#Day 1
+#Question 1 :
+'''- asks user how much money do you have with them
+- asks how much percent of it they would like to spend
+- then take cost of items they have bought seprately ( assume they only take 3 items)
+- if the total cost is less than the amout they can spend , tell them the can buy it
+- else , tell the they can buy it'''
 money = float(input("How much money do you have? "))
 percent= float(input("What percentage of your money would you like to spend? "))
 limit = (percent/ 100) * money
