9a83df0cd1136a9ddec29e94936308cab42d5f60
Adithyan H <adityenh@gmail.com>
2024-12-10 05:53:50 +0530

    Updated Task 2.3,Bonus Question.py

--- Task 2.3,Bonus Question.py
+++ Task 2.3,Bonus Question.py
@@ -8,35 +8,28 @@ while True:
     print("3.remove name")
     print("4.show names")
     print("5.exit")
-
     command = input("command: ")
-
     if command == "1":
         name = input("enter name: ")
         names.append(name)
-
     if command == "2":
         name = input("enter name: ")
         if name in names:
             print("name is present")
         else:
             print("name is not present")
-
     if command == "3":
         name = input("enter name: ")
         if name in names:
             names.remove(name)
         else:
             print("name not found")
-
     if command == "4":
         print("the names are:")
         for name in names:
             print(name)
-
     if command == "5":
         print("Program exited")
         break
-
     if command not in ["1", "2", "3", "4", "5"]:
         print("invalid command")
